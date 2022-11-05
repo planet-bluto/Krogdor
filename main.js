aff4c294535ceca8c5bda37ab267608fff03ea2e
Blu <plush.gamer.456@gmail.com>
2022-11-05 01:16:04 -0400

    General update
    Added console commands, sends messages on ping, and has a sleep mode (spam prevention)

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+// CONSTANTS:
+// ***************************
 const print = console.log
 require('dotenv').config({ path: `${__dirname}/.env` })
 const token = process.env['token']
@@ -21,11 +23,29 @@ const {Client, IntentsBitField} = require('discord.js')
 var bits = Object.values(IntentsBitField.Flags).filter(val => { return Number.isInteger(val)})
 const client = new Client({ intents: bits })
 
+// singlular variable
+var sleep_mode = false
+//
+//
+//
+//
+// FUNCTIONS:
+// ***************************
 var attempt = async (rigged = false) => {
+  var channel = await client.channels.fetch(DEFAULT)
+
+  // NO DEAD CHAT 💀
+  let lastMessageCollect = await channel.messages.fetch({ limit: 1 })
+  let lastMessage = lastMessageCollect.first()
+  let timeDiff = Math.abs(lastMessage.createdTimestamp - Date.now())
+  if (timeDiff > 3600000) { print("chat is dead, I'ma wait..."); sleep_mode = true; return }
+
+  if (sleep_mode == true) {
+    print("Oh, chat is back!")
+    sleep_mode = false
+  }
   let randVal = randi(1, 50)
-  print(rigged)
   if (randVal == 1 || rigged) {
-    var channel = await client.channels.fetch(DEFAULT)
     channel.send("THERE'S A BOMB STRAPPED TO MY CHEST")
     print("BOMBINGS")
   }
@@ -35,11 +55,22 @@ var send = async (id, content) => {
   var channel = await client.channels.fetch(id)
   channel.send(content)
 }
-
+//
+//
+//
+//
+// LISTENERS:
+// ***************************
 client.on('ready', () => {
   print(`${client.user.username}.`)
   attempt()
   setInterval(attempt, 30000)
+})
+
+client.on('messageCreate', message => {
+  if (message.channel == DEFAULT && message.mentions.users.has(client.user.id)) {
+    attempt(true)
+  }
 })
 
 process.stdin.on("data", data => {
@@ -55,5 +86,10 @@ process.stdin.on("data", data => {
     break;
   }
 })
-
+//
+//
+//
+//
+// LOG INTO Krogdor:
+// ***************************
 client.login(token);
