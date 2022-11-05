64cb55d7b3ca6a0a4cee47d986b47e32fad2cdd0
Blu <plush.gamer.456@gmail.com>
2022-11-04 23:56:21 -0400

    Uses SUPERIOR method of getting terminal input

--- main.js
+++ main.js
@@ -2,6 +2,14 @@ const print = console.log
 require('dotenv').config({ path: `${__dirname}/.env` })
 const token = process.env['token']
 
+const readline = require('readline')
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+    historySize: 30,
+    prompt: '$ '
+})
+
 const randi = (min, max) => {
   min = Math.ceil(min);
   max = Math.floor(max);
@@ -12,18 +20,27 @@ const {Client, IntentsBitField} = require('discord.js')
 var bits = Object.values(IntentsBitField.Flags).filter(val => { return Number.isInteger(val)})
 const client = new Client({ intents: bits })
 
+var attempt = async (rigged = false) => {
+  let randVal = randi(1, 50)
+  if (randVal == 1 || rigged) {
+    var channel = await client.channels.fetch('518671807503532062')
+    channel.send("THERE'S A BOMB STRAPPED TO MY CHEST")
+    print("BOMBINGS")
+  }
+}
+
 client.on('ready', () => {
   print(`${client.user.username}.`)
-  let attempt = async () => {
-    let randVal = randi(1, 50)
-    if (randVal == 1) {
-      var channel = await client.channels.fetch('518671807503532062')
-      channel.send("THERE'S A BOMB STRAPPED TO MY CHEST")
-      print("BOMBINGS")
-    }
-  }
   attempt()
   setInterval(attempt, 30000)
+})
+
+process.stdin.on("data", data => {
+  var input = data.toString()
+  print(`"${input}"`)
+  if (input == "send") {
+    attempt()
+  }
 })
 
 client.login(token);
