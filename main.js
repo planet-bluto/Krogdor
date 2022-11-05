bc4f0b2c5cbe8e50a41777b9025fe1bf821bcc9b
Blu <plush.gamer.456@gmail.com>
2022-11-05 00:45:05 -0400

    Now it's rigged >:)

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 const print = console.log
 require('dotenv').config({ path: `${__dirname}/.env` })
 const token = process.env['token']
+const DEFAULT = "518671807503532062"
 
 const readline = require('readline')
 const rl = readline.createInterface({
@@ -22,11 +23,17 @@ const client = new Client({ intents: bits })
 
 var attempt = async (rigged = false) => {
   let randVal = randi(1, 50)
+  print(rigged)
   if (randVal == 1 || rigged) {
-    var channel = await client.channels.fetch('518671807503532062')
+    var channel = await client.channels.fetch(DEFAULT)
     channel.send("THERE'S A BOMB STRAPPED TO MY CHEST")
     print("BOMBINGS")
   }
+}
+
+var send = async (id, content) => {
+  var channel = await client.channels.fetch(id)
+  channel.send(content)
 }
 
 client.on('ready', () => {
@@ -36,10 +43,16 @@ client.on('ready', () => {
 })
 
 process.stdin.on("data", data => {
-  var input = data.toString()
-  print(`"${input}"`)
-  if (input == "send") {
-    attempt()
+  var input = data.toString().trim()
+  var args = input.split(" ")
+  var cmd = args.shift()
+  switch (cmd) {
+    case 'bomb':
+      attempt(true)
+    break;
+    case 'send':
+      send(DEFAULT, args.join(" "))
+    break;
   }
 })
 
